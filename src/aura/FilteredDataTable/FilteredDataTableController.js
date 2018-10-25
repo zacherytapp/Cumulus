3e6de57e2d06fd981747c84b9ba80db541e60df5
Beth Breisnes <bbreisnes@salesforce.com>
2018-10-25 15:47:55 -0700

    columns based on field set

--- src/aura/FilteredDataTable/FilteredDataTableController.js
+++ src/aura/FilteredDataTable/FilteredDataTableController.js
@@ -2,44 +2,31 @@
 
     doInit: function(component, event, helper) {
 
-        var columns = [
-            {label: 'Opportunity Name', fieldName: 'Name', type: 'text'},
-            {label: 'Stage', fieldName: 'StageName', type: 'text'},
-            {label: 'Close Date', fieldName: 'CloseDate', type: 'date'},
-            {label: 'Primary Contact', fieldName: 'Primary_Contact__c', type: 'lookup'},
-            {label: 'Amount', fieldName: 'Amount', type: 'currency'}
-        ];
-
-        component.set('v.columns', columns);
-        var fields = [];
-        columns.forEach(function(col){
-           fields.push(col.fieldName);
-        });
-        
         var sObjectName = component.get('v.sObjectName');
         var recordId = component.get('v.recordId');
         var whereClause = component.get('v.whereClause');
         var childObject = component.get('v.childSObjectName');
         var relationshipField = component.get('v.relationshipField');
+        var fieldSet = component.get('v.fieldSet');
 
         var action = component.get('c.getData');
 
         action.setParams({
             sObjectName: sObjectName,
-            fields: fields,
             recordId: recordId,
             whereClause: whereClause,
             childObject: childObject,
-            relationshipField: relationshipField
+            relationshipField: relationshipField,
+            fieldSet: fieldSet
         });
 
         action.setCallback(this, function (response) {
             var state = response.getState();
 
             if (state === "SUCCESS") {
-                var data = response.getReturnValue();
-                console.log(data);
-                component.set('v.data', data);
+                var response = JSON.parse(response.getReturnValue());
+                component.set('v.data', response.data);
+                component.set('v.columns', response.columns);
             }
             else if (state === "ERROR") {
                 var errors = response.getError();
